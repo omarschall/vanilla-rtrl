403629ed7d1ade6cad4713e1fc156c8605d2beb9
omarschall <owen.marschall@gmail.com>
2021-05-18 11:07:45 -0400

    Add learning algorithms init file

--- learning_algorithms/__init__.py
+++ learning_algorithms/__init__.py
@@ -2,6 +2,7 @@ from .DNI import DNI
 from .Efficient_BPTT import Efficient_BPTT
 from .Future_BPTT import Future_BPTT
 from .KeRNL import KeRNL
+from .KF_RTRL import KF_RTRL
 from .Learning_Algorithm import Learning_Algorithm
 from .List_of_Gradients import List_of_Gradients
 from .Only_Output_Weights import Only_Output_Weights
