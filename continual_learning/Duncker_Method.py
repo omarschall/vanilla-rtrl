d765508536941ba7ccf2e14aa35b616d85f8ef37
omarschall <owen.marschall@gmail.com>
2021-05-21 11:09:07 -0400

    Comment CL methods

--- continual_learning/Duncker_Method.py
+++ continual_learning/Duncker_Method.py
@@ -1,14 +1,38 @@
 import sys, os
 sys.path.append(os.path.abspath('..'))
-from utils import get_Duncker_projections, split_weight_matrix
+from utils import split_weight_matrix
 import numpy as np
 from core import Simulation
 from continual_learning.Continual_Learning import Continual_Learning
 
 class Duncker_Method(Continual_Learning):
+    """CL Method that preferentially updates parameters in directions
+        that minimally interfere with subspaces used on previous subtasks.
+
+        Ref: Duncker and Sussillo, NeurIPS 2020."""
 
     def __init__(self, rnn, N_proj_data=500, mode='combined',
                  combined_task=None, proj_tasks=None):
+        """Initialize instance by specifyiing RNN to be trained, amount of
+        data to use in projections, and method for doing so.
+
+        Args:
+            rnn (core.RNN): The RNN instance undergoing continual learning.
+            N_proj_data (int): Number of time steps of data to use for
+                computing projection matrices.
+            mode (str): One of 'combined', 'previous' or 'training':
+                'combined' -> Combine all component tasks in a new training
+                    simulation to collect projection data.
+                'previous' -> Combine all component tasks *the network has
+                    seen so far* in new training simulation to collect
+                    projection data.
+                'training' -> Use last N_proj_data time steps of the actual
+                    training activity rather than separate test data (this is
+                    the method used in original paper).
+            combined_task (gen_data.Task): The task to be used if method is
+                'combined'.
+            proj_tasks (list): List of gen_data.Task instances to be used
+                sequentially at each task break if method is 'previous'."""
 
         assert mode in ['combined', 'previous', 'training']
 
@@ -25,10 +49,14 @@ class Duncker_Method(Continual_Learning):
         self.out_proj_mats = [np.eye(rnn.n_out), np.eye(rnn.n_h + 1)]
 
     def mini_update(self, sim):
+        """No update necessary at each time step"""
 
         pass
 
     def task_switch_update(self, sim):
+        """By one of the three methods, input and hidden state data matrices
+        are generated or accessed, which are used to compute the projection
+        matrices that the gradient is ultimately passed through."""
 
         self.n_switches += 1
 
@@ -57,8 +85,8 @@ class Duncker_Method(Continual_Learning):
 
         elif self.mode == 'training':
 
-            A = np.array(sim.mons['rnn.a'][-N_proj_data:])
-            X = np.array(sim.mons['rnn.x'][-N_proj_data:])
+            A = np.array(sim.mons['rnn.a'][-self.N_proj_data:])
+            X = np.array(sim.mons['rnn.x'][-self.N_proj_data:])
 
             if not hasattr(self, 'A_Duncker'):
                 self.A_Duncker = A
@@ -67,15 +95,40 @@ class Duncker_Method(Continual_Learning):
                 self.A_Duncker = np.vstack([self.A_Duncker, A])
                 self.X_Duncker = np.vstack([self.X_Duncker, X])
 
-        P_z, P_wz, P_h, P_y = get_Duncker_projections(self.A_Duncker,
-                                                      self.X_Duncker,
-                                                      rnn=self.rnn,
-                                                      n_switches=self.n_switches)
+        P_z, P_wz, P_h, P_y = self.get_projections(self.A_Duncker,
+                                                   self.X_Duncker,
+                                                   n_switches=self.n_switches)
 
         self.rec_proj_mats = [P_wz, P_z]
-        self.out_proj_mats=[P_y, P_h]
+        self.out_proj_mats = [P_y, P_h]
+
+    def get_projections(self, A, X, n_switches=1, inv_constant=0.001):
+        """Get the continual learning projections from Duncker et al. given the
+        data that should be included and the current parameters of the rnn."""
+
+        rnn = self.rnn
+
+        m = rnn.n_h + rnn.n_in + 1
+        Z = np.concatenate([A, X, np.ones((A.shape[0], 1))], axis=1).T
+        W = np.concatenate([rnn.W_rec,
+                            rnn.W_in,
+                            rnn.b_rec.reshape((-1, 1))], axis=1)
+        WZ = W.dot(Z)
+
+        P_z = np.linalg.inv(Z.dot(Z.T) / (n_switches * inv_constant) + np.eye(m))
+        P_wz = np.linalg.inv(WZ.dot(WZ.T) / (n_switches * inv_constant) + np.eye(rnn.n_h))
+
+        H = np.concatenate([A, np.ones((A.shape[0], 1))], axis=1).T
+        W_o = np.concatenate([rnn.W_out, rnn.b_out.reshape((-1, 1))], axis=1)
+        WH = W_o.dot(H)
+
+        P_h = np.linalg.inv(H.dot(H.T) / (n_switches * inv_constant) + np.eye(rnn.n_h + 1))
+        P_y = np.linalg.inv(WH.dot(WH.T) / (n_switches * inv_constant) + np.eye(rnn.n_out))
+
+        return P_z, P_wz, P_h, P_y
 
     def __call__(self, grads_list):
+        """Project gradients through matrices computed via get_projections"""
 
         grads = grads_list
 
--- continual_learning/Synaptic_Intelligence.py
+++ continual_learning/Synaptic_Intelligence.py
@@ -1,12 +1,23 @@
 import sys, os
 sys.path.append(os.path.abspath('..'))
 import numpy as np
-from utils import get_Duncker_projections, split_weight_matrix
 from continual_learning.Continual_Learning import Continual_Learning
 
 class Synaptic_Intelligence(Continual_Learning):
+    """CL Method that preferentially weights each parameter by its importance
+    for learning each previous task
+
+    Ref: Zenke and Ganguli, PMLR 2017"""
 
     def __init__(self, rnn, c, epsilon=0.001):
+        """Initialize by specifying RNN instance, regularization constant c,
+        and stability constant epsilon.
+
+        Args:
+            rnn (core.RNN): Instance of RNN undergoing continual learning.
+            c (float): Constant scaling overall strength of SI regularization.
+            epsilon (float): Small positive constant to ensure invertability.
+        """
 
         self.rnn = rnn
         self.c = c
@@ -18,6 +29,12 @@ class Synaptic_Intelligence(Continual_Learning):
         self.SI_omega = [np.zeros(s) for s in self.rnn.shapes]
 
     def mini_update(self, sim):
+        """Update the 'importance' omega of each parameter for the *current*
+        task at each time step.
+
+        The importance omega is the average lr-scaled update direction
+        (the optimizer velocity) with the gradient as computed by the learning
+        algorithm, updated online at each time step."""
 
         #Update omegas
         for i_param in range(len(self.SI_omega)):
@@ -26,22 +43,23 @@ class Synaptic_Intelligence(Continual_Learning):
             self.SI_omega[i_param] -= v * g
 
     def task_switch_update(self, sim):
+        """Update the Omega quantity for each parameter by adding parameter
+        importance omega scaled by overall change SI_Delta. Reset omega."""
 
-        #Refresh omegas, update Omegas
-        if sim.task_marker[sim.i_t] != sim.task_marker[sim.i_t - 1]:
+        self.SI_Theta.append([p.copy() for p in self.rnn.params])
+        self.SI_Delta = [p - q for p, q in zip(self.SI_Theta[-1],
+                                               self.SI_Theta[-2])]
 
-            self.SI_Theta.append([p.copy() for p in self.rnn.params])
-            self.SI_Delta = [p - q for p, q in zip(self.SI_Theta[-1],
-                                                   self.SI_Theta[-2])]
+        for i_param in range(len(self.SI_omega)):
+            omega = self.SI_omega[i_param]
+            Delta = self.SI_Delta[i_param]
+            self.SI_Omega[i_param] += omega / (Delta**2 + self.epsilon)
 
-            for i_param in range(len(self.SI_omega)):
-                omega = self.SI_omega[i_param]
-                Delta = self.SI_Delta[i_param]
-                self.SI_Omega[i_param] += omega / (Delta**2 + self.epsilon)
-
-            self.SI_omega = [np.zeros(s) for s in self.rnn.shapes]
+        self.SI_omega = [np.zeros(s) for s in self.rnn.shapes]
 
     def __call__(self, grads_list):
+        """Compute derivative of regularized loss and add to gradients as
+        computed by learning algorithm."""
 
         new_grads = []
         diff = []
