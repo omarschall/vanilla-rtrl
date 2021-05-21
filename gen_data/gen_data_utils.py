8563cf70ce806ec9fa55a78913f7f8dd946421f1
omarschall <owen.marschall@gmail.com>
2021-05-21 16:45:56 -0400

    Add gen_data_utils.py

---
+++ gen_data/gen_data_utils.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def generate_ergodic_markov_task(one_hot_dim=4, n_inputs=4, idem=True):
+    """Generates a random set of fixed_points as one hots and associated
+    transitions under idempotency constraint (or not) until it finds an
+    ergodic instance."""
+
+    FPs = [np.eye(one_hot_dim)[i] for i in range(one_hot_dim)]
+    n_states = len(FPs)
+
+    done = False
+
+    while not done:
+
+        T_dict = {}
+
+        for i_input in range(n_inputs):
+
+            T = np.zeros((n_states, n_states))
+            perm = np.random.permutation(range(n_states))
+
+            for i_fp in range(n_states):
+                T[i_fp, perm[i_fp]] = 1
+
+            if idem:
+                #force to be idempotent
+                for i_fp in np.random.permutation(range(n_states)):
+                    if T[:,i_fp].sum() > 0:
+                        T[i_fp, :] = np.eye(n_states)[i_fp]
+
+                #check if idempotent
+                assert (T == T.dot(T)).all()
+
+            T_dict['input_{}'.format(i_input)] = T
+
+        T_avg = sum([T for T in T_dict.values()]) / len(T_dict.keys())
+
+
+        T_power = np.round(np.linalg.matrix_power(T_avg, 1000), 2)
+
+        if (T_power[0] > 0).all():
+            done = True
+
+    return FPs, T_dict
