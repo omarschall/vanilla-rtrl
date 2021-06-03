a95be7447a84aca26667f5f3f268408c24625c9e
omarschall <owen.marschall@gmail.com>
2021-06-03 10:14:06 -0400

    Add comments, add close jupyter file, update init, refactor sensibly

--- cluster/start_jupyter_notebook.py
+++ cluster/start_jupyter_notebook.py
@@ -1,14 +1,26 @@
 import subprocess
-from sync import sync_cluster
+from sync_cluster import sync_cluster
 import webbrowser
 import appscript
 import time
 
-def start_jupyter_notebook(home_path='/Users/omarschall/vanilla-rtrl/',
+def start_jupyter_notebook(local_path='/Users/omarschall/vanilla-rtrl/',
                            scratch_path='/scratch/oem214/vanilla-rtrl/',
                            username='oem214', domain='greene.hpc.nyu.edu'):
+    """For given user data, opens a jupyter notebook on the cluster, accesses
+    port/token info, and opens a web browser with jupyter notebook running.
 
-    sync_cluster(home_path=home_path, scratch_path=scratch_path,
+    Requires:
+    (1) Singularity files on cluster to use proper python environment
+    (2) jupyter.s file that submits a job with name "jupyter", accesses the
+        Singularity, and runs jupyter notebook to output file jupyter.o .
+
+    Starts by updating cluster with local python code base. Then clears
+    old jupyter.o files, runs jupyter notebook via sbatch, continuosly checks
+    for relevant connection info (port and token) in jupyter.o file, and when
+    available opens a browser connecting to this notebook job."""
+
+    sync_cluster(local_path=local_path, scratch_path=scratch_path,
                  username=username, domain=domain)
     remote = '{}@{}'.format(username, domain)
 
