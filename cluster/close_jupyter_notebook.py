9a58f28d5dd156df7bdf9cbc7f69967d349125ed
omarschall <owen.marschall@gmail.com>
2021-06-08 13:56:21 -0400

    Clean up __main__ issue

--- cluster/close_jupyter_notebook.py
+++ cluster/close_jupyter_notebook.py
@@ -6,6 +6,3 @@ def close_jupyter_notebook(username='oem214', domain='greene.hpc.nyu.edu'):
     remote = '{}@{}'.format(username, domain)
     #Cancel all jobs for your user with name "jupyter"
     subprocess.run(['ssh', remote, 'scancel', '-n', 'jupyter', '-u', username])
-
-if __name__ == '__main__':
-    close_jupyter_notebook()
