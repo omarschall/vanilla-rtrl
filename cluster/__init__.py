40bbae92c9670d1f3dc192e8f6c098ad0ba0b8c6
omarschall <owen.marschall@gmail.com>
2021-06-09 18:14:26 -0400

    Fix init import

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -1,3 +1,4 @@
 from .close_jupyter_notebook import close_jupyter_notebook
 from .start_jupyter_notebook import start_jupyter_notebook
+from .submit_jobs import write_job_file, submit_job
 from .sync_cluster import sync_cluster
