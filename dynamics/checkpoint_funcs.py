0738d9afdde90d3271c09e7385f7f5be455029b6
omarschall <owen.marschall@gmail.com>
2021-11-22 11:04:04 -0500

    Add multiple next biggest eigenvalues evaluators

--- dynamics/checkpoint_funcs.py
+++ dynamics/checkpoint_funcs.py
@@ -12,15 +12,31 @@ def get_checkpoint_spectral_radius(checkpoint):
 
     return np.amax(np.absolute(eigs))
 
+def get_checkpoint_2nd_eigenvalue(checkpoint):
+    """Gets checkpoint spectral radius"""
+
+    eigs, _ = np.linalg.eig(checkpoint['rnn'].W_rec)
+    eigs_sorted = sorted(np.absolute(eigs))[::-1]
+
+    return eigs_sorted[1]
+
+def get_checkpoint_3nd_eigenvalue(checkpoint):
+    """Gets checkpoint spectral radius"""
+
+    eigs, _ = np.linalg.eig(checkpoint['rnn'].W_rec)
+    eigs_sorted = sorted(np.absolute(eigs))[::-1]
+
+    return eigs_sorted[2]
+
 def get_checkpoint_weight_std(checkpoint):
     """Gets checkpoint spectral radius"""
 
-    return checkpoint['W_rec'].std()
+    return checkpoint['rnn'].W_rec.std()
 
 def get_checkpoint_weight_mean(checkpoint):
     """Gets checkpoint spectral radius"""
 
-    return checkpoint['W_rec'].mean()
+    return checkpoint['rnn'].W_rec.mean()
 
 def get_checkpoint_n_stable_FPs(checkpoint):
     """Gets the number of stable checkpoint nodes after topological analysis."""
@@ -31,3 +47,20 @@ def get_checkpoint_n_unstable_FPs(checkpoint):
     """Gets the number of unstable fixed points."""
 
     return checkpoint['cluster_means'].shape[0] - checkpoint['nodes'].shape[0]
+
+def get_checkpoint_W_rec_dim(checkpoint):
+    """Gets the latent dimensionality of the network as measured by
+    participation coefficient of the recurrent weights themselves."""
+
+    W_rec = checkpoint['rnn'].W_rec
+
+    eigs, _ = np.linalg.eig(W_rec)
+
+    abs_eigs = np.abs(eigs)
+
+    return np.square(np.sqrt(abs_eigs).sum()) / abs_eigs.sum()
+
+def get_checkpoint_participation_coefficient(checkpoint):
+    """Gets the participation coefficient of the network test activity."""
+
+    return checkpoint['participation_coef']
