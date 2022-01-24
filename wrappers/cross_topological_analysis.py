a3cea3f21b42d34de2335c7a7d663033ff5baa73
omarschall <owen.marschall@gmail.com>
2022-01-23 23:56:29 -0500

    Fix write file results subdir bug

--- wrappers/cross_topological_analysis.py
+++ wrappers/cross_topological_analysis.py
@@ -63,6 +63,7 @@ def cross_topological_analysis(saved_run_root_name,
         analysis_job_name = 'analyze_{}'.format(saved_run_name)
 
         write_job_file(analysis_job_name, py_file_name='analyze_main.py',
+                       results_subdir=results_subdir,
                        py_args='--name {}'.format(saved_run_name), ppn=ppn)
         get_ipython().system('cp {} {}'.format(analyze_main_path, cluster_main_dir))
         analysis_job_id = submit_job('../job_scripts/{}.s'.format(analysis_job_name),
@@ -79,6 +80,7 @@ def cross_topological_analysis(saved_run_root_name,
     analysis_id_dependency = ':'.join([str(id_) for id_ in analysis_job_ids])
 
     write_job_file(compare_job_name, py_file_name='cross_compare_main.py',
+                   results_subdir=results_subdir,
                    py_args='--name {}'.format(saved_run_root_name))
     get_ipython().system('cp {} {}'.format(compare_main_path, cluster_main_dir))
     submit_job('../job_scripts/{}.s'.format(compare_job_name),
