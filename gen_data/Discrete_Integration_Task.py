74aa9fe36daf45cb233f9d478acb969f4d2eb5f5
omarschall <owen.marschall@gmail.com>
2022-02-02 13:59:26 -0500

    Attempt to add uniform stats version

--- gen_data/Discrete_Integration_Task.py
+++ gen_data/Discrete_Integration_Task.py
@@ -1,11 +1,14 @@
 import numpy as np
 from gen_data.Task import Task
+from math import floor
 
 class Discrete_Integration_Task(Task):
     """Class for the 1-dimensional discrete integration task."""
 
     def __init__(self, p_bit=0.05, p_reset=0.005, tau_task=1,
-                 reset_mode='random', report_count=False):
+                 reset_mode='random', report_count=False,
+                 uniform_count_stats=False, max_count=5,
+                 max_total_inputs=12):
         """Later
 
         Args:
@@ -25,8 +28,11 @@ class Discrete_Integration_Task(Task):
         self.probe_inputs = [np.array([1, 0]),
                              np.array([-1, 0]),
                              np.array([0, 1])]
+        self.uniform_count_stats = uniform_count_stats
+        self.max_count = max_count
+        self.max_total_inputs = max_total_inputs
 
-    def gen_dataset(self, N):
+    def gen_dataset_1(self, N):
 
         #N = N // self.tau_task
 
@@ -59,3 +65,57 @@ class Discrete_Integration_Task(Task):
             t_reset_prev = t_reset
 
         return X, Y, None
+
+    def get_dataset_2(self, N):
+
+        period = int(1 / self.p_reset)
+        N_trials = N // period
+
+        X = []
+        Y = []
+
+        for i_trial in range(N_trials):
+            final_net_count = np.random.uniform([-self.max_count,
+                                                 self.max_count])
+            min_positive = np.maximum(0, final_net_count)
+            max_positive = 0.5 * floor(self.max_total_inputs + final_net_count)
+            final_positive_count = np.random.randint(low=min_positive,
+                                                     high=max_positive + 1)
+            final_negative_count = final_positive_count - final_net_count
+
+            #Randomly put these positive and negative counts into boxes
+            x_trial = np.zeros((period, 2))
+            time_indices = list(range(period))
+            positive_input_indices = np.random.choice(time_indices,
+                                                      size=final_positive_count,
+                                                      replace=False)
+            time_indices = [t for t in time_indices
+                            if t not in positive_input_indices]
+            negative_input_indices = np.random.choice(time_indices,
+                                                      size=final_negative_count,
+                                                      replace=False)
+
+            x_trial[positive_input_indices, 0] = 1
+            x_trial[negative_input_indices, 0] = -1
+            x_trial[0, 1] = 1
+
+            X.append(x_trial)
+
+            y_trial = np.zeros((period, 2))
+            y_trial[:, 0] = np.cumsum(x_trial[:, 0])
+            if not self.report_count:
+                y_trial[:, 0] = np.sign(y_trial[:, 0])
+
+            Y.append(y_trial)
+
+        X = np.concatenate(X, axis=0)
+        Y = np.concatenate(Y, axis=0)
+
+        return X, Y, None
+
+    def gen_dataset(self, N):
+
+        if self.uniform_count_stats:
+            return self.get_dataset_1(N)
+        else:
+            return self.gen_dataset_2(N)
