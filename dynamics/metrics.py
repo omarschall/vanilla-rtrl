ffaaa92749d6779208582b727c3f2ef3a0e3dfdb
omarschall <owen.marschall@gmail.com>
2022-02-03 22:48:37 -0500

    Add post processing to stage assignments

--- dynamics/metrics.py
+++ dynamics/metrics.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def assign_time_points_to_stages(signal_dict, performance_criterion,
-                                 topological_criterion, window_duration=10):
+                                 topological_criterion, window_duration=10,
+                                 post_process=True, time_point_trigger=4):
     """Takes a dictionary of signals, which must include 'test_loss' and
     'aligned_graph_distances', and returns an array of stage assignments.
 
@@ -57,5 +58,51 @@ def assign_time_points_to_stages(signal_dict, performance_criterion,
 
     stage_assignments = stage_assignments.astype(np.int)
 
+    if post_process:
+        stage_assignments = post_process_stage_assignments(stage_assignments,
+                                                           time_point_trigger=time_point_trigger)
+
     return stage_assignments
 
+def post_process_stage_assignments(stage_assignments, time_point_trigger=4):
+    """For a given set of stage assignments, performs post processing by making
+    each period contiguous.
+
+    Starting with stage 2, we look for the first time point with 4 following
+    consecutive time points that were initially categorized as stage 2. Then
+    this is considered the "transition point," and we repeat for stage 3 and 4.
+    """
+
+    #Copy original stage assignments
+    ret = stage_assignments.copy()
+    t_stage_transition_prev = 0
+
+    for i_stage in [2, 3, 4]:
+
+        #Find points assigned to stage i_stage
+        stage_i_points = (stage_assignments == i_stage)
+
+        #Transition times
+        stage_i_transitions = (stage_assignments == i_stage)
+
+        #Roll array a number of time steps forward as specified by time_point_trigger
+        for i_roll in range(1, time_point_trigger):
+            stage_roll = np.roll(stage_i_points, -i_roll)
+            stage_i_transitions = np.logical_and(stage_i_transitions,
+                                                 stage_roll)
+
+        try:
+            t_stage_transition = sorted(np.where(stage_i_transitions)[0])[0]
+        except IndexError:
+            t_stage_transition = None
+
+        #Put everything up to stage n transition in stage n-1
+        if t_stage_transition_prev is not None:
+            ret[t_stage_transition_prev:t_stage_transition] = i_stage - 1
+
+        t_stage_transition_prev = t_stage_transition
+
+    if t_stage_transition_prev is not None:
+        ret[t_stage_transition_prev:] = 4
+
+    return ret
