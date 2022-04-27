e526530a3e19c7b37270236187af475f8371257a
omarschall <owen.marschall@gmail.com>
2022-04-27 11:40:56 -0400

    Fully change new input aesthetics

--- plotting/checkpoint_plots.py
+++ plotting/checkpoint_plots.py
@@ -40,113 +40,7 @@ def plot_output_from_checkpoint(checkpoint, data, plot_title=None,
 
     return fig
 
-def plot_input_dependent_topology(checkpoint, reference_checkpoint=None,
-                                  graph_key='adjmat',
-                                  i_input=None, plotting_noise=0.05,
-                                  return_fig=False, n_inputs=None,
-                                  color_scheme='dotted',
-                                  colors=None,
-                                  circle_radius=0.18):
-
-    if n_inputs is None:
-        n_in = 6
-    else:
-        n_in = n_inputs
-
-    if colors is None:
-        colors = ['C{}'.format(i) for i in range(n_in)]
-
-    n_half = n_in // 2
-
-    if i_input is not None:
-        keys = ['{}_input_{}'.format(graph_key, i_input)]
-    else:
-        keys = ['{}_input_{}'.format(graph_key, i) for i in range(n_in)]
-
-    n_nodes = checkpoint[keys[0]].shape[0]
-
-    if reference_checkpoint is not None:
-        total_nodes = max(reference_checkpoint['nodes'].shape[0], n_nodes)
-    else:
-        total_nodes = n_nodes
-
-    fig = plt.figure(figsize=(4, 4))
-    plt.title('Checkpoint {}'.format(checkpoint['i_t']))
-
-    t_range = np.arange(np.pi, -np.pi, -2 * np.pi / total_nodes)
-    circle_nodes = np.array([[np.cos(t), np.sin(t)] for t in t_range[:n_nodes]])
-
-    #plot little circles
-    theta = np.arange(0, 2 * np.pi, 0.01)
-    for node in circle_nodes:
-        plt.plot(node[0] + circle_radius * np.cos(theta),
-                 node[1] + circle_radius * np.sin(theta),
-                 color=('0.6'))
-
-    np.random.seed(0)
-    leg = []
-    for key in keys:
-
-        graph = checkpoint[key]
-        i_input_ = int(key.split('_')[-1])
-
-        if color_scheme == 'dotted':
-            i_color = i_input_ % 3
-            linestyle = ['-', '--'][i_input_ // n_half]
-        if color_scheme == 'different':
-            i_color = i_input_
-            linestyle = '-'
-
-        leg_ = 'Input {}'.format(i_color)
-        if linestyle == '-':
-            leg_ += ', +'
-        else:
-            leg_ += ', -'
-        leg.append(leg_)
-
-        for i, j in zip(*np.where(graph != 0)):
-
-            if i == j:
-                continue
-
-            weight = graph[i, j]
-
-            if weight < 0.05:
-                continue
-
-            line = np.array([circle_nodes[i], circle_nodes[j]])
-
-            start = circle_nodes[i] + np.random.normal(0, plotting_noise, 2)
-            end = circle_nodes[j] + np.random.normal(0, plotting_noise, 2)
-
-            plt.plot([start[0], end[0]],
-                      [start[1], end[1]],
-                      color=colors[i_color], alpha=weight,
-                      linestyle=linestyle)
-
-            plt.plot([start[0]],
-                      [start[1]], 'x',
-                      color=colors[i_color], alpha=weight)
-
-            plt.plot([end[0]],
-                      [end[1]], '.',
-                      color=colors[i_color], alpha=weight)
-
-            # plt.arrow(start[0], start[1], end[0] - start[0], end[1] - start[1],
-            #           color='C{}'.format(i_color), alpha=weight,
-            #           head_width=0.025, head_length=0.05,
-            #           linestyle=linestyle)
-
-    plt.xlim([-1.4, 1.4])
-    plt.ylim([-1.4, 1.4])
-    #plt.legend(leg)
-    #plt.axis('equal')
-    #set_trace()
-
-    if return_fig:
-        return fig
-
-def plot_input_dependent_topolog2(checkpoint, data,
+def plot_input_dependent_topology(checkpoint, data,
                                   reference_checkpoint=None,
                                   graph_key='adjmat',
                                   i_input=None, plotting_noise=0.05,
@@ -157,7 +51,8 @@ def plot_input_dependent_topolog2(checkpoint, data,
                                   space_range=1,
                                   circle_color=('0.8'),
                                   circle_radius=0.18,
-                                  output_ylim=[-9, 3]):
+                                  output_ylim=[-9, 3],
+                                  figsize=(8, 4)):
 
     if n_inputs is None:
         n_in = 6
@@ -178,7 +73,7 @@ def plot_input_dependent_topolog2(checkpoint, data,
     else:
         total_nodes = n_nodes
 
-    fig, ax = plt.subplots(1, 2, figsize=(8, 4))
+    fig, ax = plt.subplots(1, 2, figsize=figsize)
     fig.suptitle('Checkpoint {}'.format(checkpoint['i_t']))
 
     t_range = np.arange(np.pi, -np.pi, -2 * np.pi / total_nodes)
@@ -205,7 +100,7 @@ def plot_input_dependent_topolog2(checkpoint, data,
     for key in keys:
         i_input_ = int(key.split('_')[-1])
         if color_scheme == 'dotted':
-            i_color = i_input_ % 3
+            i_color = i_input_ % n_half
             linestyle = ['-', '--'][i_input_ // n_half]
         if color_scheme == 'different':
             i_color = i_input_
@@ -274,7 +169,7 @@ def plot_input_dependent_topolog2(checkpoint, data,
     test_sim = Simulation(rnn)
     test_sim.run(data, mode='test', monitors=['rnn.y_hat'], verbose=False)
 
-    for i in range(3):
+    for i in range(rnn.n_in):
         ax[1].plot(data['test']['X'][40:, i] - i * 2.5, color=('0.7'), linestyle='--')
         ax[1].plot(data['test']['Y'][40:, i] - i * 2.5, color=('0.2'))
         ax[1].plot(test_sim.mons['rnn.y_hat'][40:, i] - i * 2.5, color=colors[i])
