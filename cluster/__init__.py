3db736ce1d87991771589d774dfe783f3202324d
omarschall <owen.marschall@gmail.com>
2023-03-01 17:34:03 -0500

    Start process of making compatible with axon

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -4,4 +4,4 @@ from .process_results import unpack_analysis_results,\
                              unpack_sparse_cross_compare_results
 from .start_jupyter_notebook import start_jupyter_notebook
 from .submit_jobs import write_job_file, submit_job, unpack_processed_data
-from .sync_cluster import sync_cluster
+from .sync_cluster import sync_cluster, sync_columbia_cluster
