21cbe193b8c0a4576b3512faea48582eea1d9482
omarschall <owen.marschall@gmail.com>
2023-03-03 11:08:56 -0500

    Tentatively done with axon login, basic cartesian product job capability

--- jupyter_notebook.py
+++ jupyter_notebook.py
@@ -1,4 +1,4 @@
-from cluster import start_jupyter_notebook, close_jupyter_notebook
+from cluster import start_jupyter_notebook, close_jupyter_notebook, start_axon_jupyter_notebook
 import argparse
 
 ### Open or close a jupyter notebook depending on arguments passed when
@@ -7,14 +7,21 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-open', dest='open_', action='store_true')
 parser.add_argument('-close', dest='close_', action='store_true')
-parser.set_defaults(open_=True, close_=False)
+parser.add_argument('-axon', dest='axon_', action='store_true')
+parser.set_defaults(open_=True, close_=False, axon_=False)
 
 args = parser.parse_args()
 
 if args.open_ and not args.close_:
 
-    start_jupyter_notebook()
+    if args.axon_:
+        start_axon_jupyter_notebook()
+    else:
+        start_jupyter_notebook()
 
 if args.close_:
 
-    close_jupyter_notebook()
+    if args.axon_:
+        close_jupyter_notebook(username='om2382', domain='axon.rc.zi.columbia.edu')
+    else:
+        close_jupyter_notebook()
